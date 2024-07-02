22c22302239b777698775b807eac6e60bf6fdba4
ranelho <ranelhol@gmail.com>
2024-07-02 16:50:39 -0300

    add retorno doc

--- src/main/java/com/rlti/contratos/contrato/application/api/ContratoApi.java
+++ src/main/java/com/rlti/contratos/contrato/application/api/ContratoApi.java
@@ -1,21 +1,21 @@
 package com.rlti.contratos.contrato.application.api;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-@RestController
 @RequestMapping("/v1/contrato")
 public interface ContratoApi {
 
     @PostMapping(value = "/cadastro")
     @ResponseStatus(code = HttpStatus.CREATED)
-    String novoContrato(@Valid @RequestBody ContratoRequest contratoRequest);
+    ResponseEntity<byte[]> novoContrato(@Valid @RequestBody ContratoRequest contratoRequest);
 
     @GetMapping(value = "/{idContrato}")
     @ResponseStatus(code = HttpStatus.OK)
-    String getContratoById(@PathVariable Long idContrato);
+    ResponseEntity<byte[]> getContratoById(@PathVariable Long idContrato);
 
     @DeleteMapping(value = "/{idContrato}")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
--- src/main/java/com/rlti/contratos/documento/DocxTextReplacer.java
+++ src/main/java/com/rlti/contratos/documento/DocxTextReplacer.java
@@ -1,14 +1,20 @@
 package com.rlti.contratos.documento;
 
+import com.itextpdf.io.source.ByteArrayOutputStream;
+import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
 import com.rlti.contratos.contrato.application.api.ContratoResponse;
 import lombok.RequiredArgsConstructor;
 import org.apache.poi.xwpf.usermodel.*;
 import org.springframework.stereotype.Component;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.List;
 import java.util.Objects;
 
@@ -16,26 +22,60 @@ import java.util.Objects;
 @Component
 public class DocxTextReplacer {
 
-    public String replaceText(ContratoResponse response) throws IOException {
-        String filePath = Objects.requireNonNull(getClass().getClassLoader()
-                        .getResource("contrato.docx"))
-                .getPath();
-        try (InputStream inputStream = new FileInputStream(filePath)) {
-            XWPFDocument doc = new XWPFDocument(inputStream);
-            replaceText(doc, "{{numero_contrato}}", response.getIdContrato().toString());
-            replaceText(doc, "{{CONTRATADA_RAZAOSOCIAL}}", response.getContratadaResponse().getNome().toUpperCase());
-            replaceText(doc, "{{CNPJ_CONTRATADA}}", response.getContratadaResponse().getCpfCnpj());
-            replaceText(doc, "{{ENDERECO_COMPLETO_COM_NUMERO_CONTRATADA}}", response.getContratadaResponse().getEnderecoContratada());
-            replaceText(doc, "{{CIDADE_CONTRATADA}}", response.getContratadaResponse().getCidadeContratada());
-            replaceText(doc, "{{ESTADO_CONTRATADA}}", response.getContratadaResponse().getEstadoContratada());
-            replaceText(doc, "{{CEP_CONTRATADA}}", response.getContratadaResponse().getCepContratada());
-            replaceText(doc, "{{CNPJ_CONTRATADA}}", response.getContratadaResponse().getCpfCnpj());
-            replaceText(doc, "{{NOME_FANTASIA_CONTRATADA}}", response.getContratadaResponse().getRazaoSocialContratada().toUpperCase());
+    /*  public String replaceText(ContratoResponse response) throws IOException {
+          String filePath = Objects.requireNonNull(getClass().getClassLoader()
+                          .getResource("contrato.docx"))
+                  .getPath();
+          try (InputStream inputStream = new FileInputStream(filePath)) {
+              XWPFDocument doc = new XWPFDocument(inputStream);
+              addDados(response, doc);
+              saveFile(filePath, doc);
+              doc.close();
+          }
+          return "ok";
+      }*/
 
-            saveFile(filePath, doc);
-            doc.close();
+    public byte[] replaceText(ContratoResponse response) throws IOException {
+        String filePath = Objects.requireNonNull(getClass().getClassLoader().getResource("contrato.docx")).getPath();
+        try (InputStream inputStream = new FileInputStream(filePath);
+             XWPFDocument doc = new XWPFDocument(inputStream);
+             ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+             substituirDados(response, doc);
+             doc.write(baos);
+             return baos.toByteArray();
         }
-        return "ok";
+    }
+
+    private void substituirDados(ContratoResponse response, XWPFDocument doc) {
+        replaceText(doc, "{{numero_contrato}}", response.getNumeroContrato().toString());
+        replaceText(doc, "{{CONTRATADA_RAZAOSOCIAL}}", response.getContratada().getNome().toUpperCase());
+        replaceText(doc, "{{CNPJ_CONTRATADA}}", response.getContratada().getCpfCnpj());
+        replaceText(doc, "{{ENDERECO_COMPLETO_COM_NUMERO_CONTRATADA}}", response.getContratada().getEnderecoContratada());
+        replaceText(doc, "{{CIDADE_CONTRATADA}}", response.getContratada().getCidadeContratada());
+        replaceText(doc, "{{ESTADO_CONTRATADA}}", response.getContratada().getEstadoContratada());
+        replaceText(doc, "{{CEP_CONTRATADA}}", response.getContratada().getCepContratada());
+        replaceText(doc, "{{CNPJ_CONTRATADA}}", response.getContratada().getCpfCnpj());
+        replaceText(doc, "{{NOME_FANTASIA_CONTRATADA}}", response.getContratada().getRazaoSocialContratada().toUpperCase());
+
+        replaceText(doc, "{{RAZAO_SOCIAL_CONTRATANTE}}", (response.getContratante().getRazaoSocialContratante() != null)
+                ? response.getContratante().getRazaoSocialContratante().toUpperCase() : response.getContratante().getNome().toUpperCase());
+        replaceText(doc, "{{CNPJ_CONTRATANTE}}", response.getContratante().getCpfCnpj());
+        replaceText(doc, "{{ENDERECO_COMPLETO_COM_NUMERO_CONTRATANTE}}", response.getContratante().getEnderecoContratante());
+        replaceText(doc, "{{CIDADE_CONTRATANTE}}", response.getContratante().getCidadeContratante());
+        replaceText(doc, "{{CEP_CONTRATANTE}}", response.getContratante().getCepContratante());
+        replaceText(doc, "{{ESTADO_CONTRATANTE}}", response.getContratante().getEstadoContratante());
+        replaceText(doc, "{{NOME_FANTASIA}}", (response.getContratante().getRazaoSocialContratante() != null)
+                ? response.getContratante().getRazaoSocialContratante().toUpperCase() : response.getContratante().getNome().toUpperCase());
+        replaceText(doc, "{{NOME_FANTASIA_CONTRATANTE}}", (response.getContratante().getRazaoSocialContratante() != null)
+                ? response.getContratante().getRazaoSocialContratante().toUpperCase() : response.getContratante().getNome().toUpperCase());
+
+        replaceText(doc, "{{data_inicio_contrato}}", response.getDataInicioContrato().toString());
+
+        replaceText(doc, "{{NOME_TESTEMUNHA1}}", response.getT1().getNome());
+        replaceText(doc, "{{CPF_TESTEMUNHA1}}", response.getT1().getCpf());
+
+        replaceText(doc, "{{NOME_TESTEMUNHA2}}", response.getT2().getNome());
+        replaceText(doc, "{{CPF_TESTEMUNHA2}}", response.getT2().getCpf());
     }
 
     private void replaceText(XWPFDocument doc, String originalText, String updatedText) {
@@ -71,5 +111,29 @@ public class DocxTextReplacer {
         }
     }
 
-   
+    public byte[] convertDocxToPdf(byte[] docxBytes) throws IOException {
+        try (InputStream inputStream = new ByteArrayInputStream(docxBytes);
+             XWPFDocument docx = new XWPFDocument(inputStream);
+             ByteArrayOutputStream pdfOutputStream = new ByteArrayOutputStream()) {
+
+            PdfWriter writer = new PdfWriter(pdfOutputStream);
+            PdfDocument pdf = new PdfDocument(writer);
+            Document document = new Document(pdf);
+
+            // Configurações adicionais, como tamanho de página e margens
+            pdf.setDefaultPageSize(new PageSize(PageSize.A4));
+
+            for (XWPFParagraph para : docx.getParagraphs()) {
+                for (XWPFRun run : para.getRuns()) {
+                    document.add(new Paragraph(run.text()));
+                }
+            }
+
+            document.close();
+            pdf.close();
+
+            return pdfOutputStream.toByteArray();
+        }
+    }
+
 }
